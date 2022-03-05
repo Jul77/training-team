3bcc59716083957095afe48eaca0cf5ec8f37072
Jul77 <kli_kli@inbox.lv>
2022-03-05 17:56:34 +0200

    Adding gradle

---
+++ src/main/java/TrainingListApplication.java
@@ -0,0 +1,11 @@
+public class TrainingListApplication {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello please make a choice:");
+
+        System.out.println("1. Add workout");
+        System.out.println("2. Get all workouts");
+        System.out.println("3. Delete workout");
+    }
+}
